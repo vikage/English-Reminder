e30dff250b50df181095e45fa440b342405530a6
Thanh Vu <thanhvu@coccoc.com>
2019-09-20 10:28:35 +0700

    Add 5 minute random word

---
+++ background.js
@@ -0,0 +1,55 @@
+var createdAlarm = false;
+chrome.runtime.onStartup.addListener(function() {
+	createAlarmIfNeed();
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+	createAlarmIfNeed();
+});
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  getWords();
+});
+
+function createAlarmIfNeed() {
+	if (createdAlarm == false) {
+		createdAlarm = true;
+		chrome.alarms.create("myAlarm", {periodInMinutes: 5} );
+	};
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function getWords() {
+	const url = chrome.runtime.getURL('data/words.json');
+	fetch(url).then((response) => response.json()).then((json) => processJson(json));
+}
+
+function processJson(json) {
+	var words = json["words"];
+	randomWords(words);
+}
+
+function randomWords(words) {
+	var index = getRndInteger(0, words.length - 1);
+	var word = words[index];
+	showNotification(word);
+}
+
+function showNotification(word) {
+	var title = word.word + " • /" + word.pronunciation + "/";
+	var message = "(" + word.category + ") • " + word.mean;
+	chrome.notifications.clear('EnglishReminder',function(){
+		console.log("Notification clear");
+	});
+	
+	chrome.notifications.create('EnglishReminder', {
+        type: 'basic',
+        iconUrl: 'icons/icon.png',
+        title: title,
+        message: message,
+        requireInteraction: true
+     }, function(notificationId) {});
+}
